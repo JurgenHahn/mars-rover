17a9f202345697e22cfec12ce0d24603aead7e0c
JurgenHahn <jurgenehahn@gmail.com>
2017-01-23 21:35:11 -0500

    some clean code......

--- rover.rb
+++ rover.rb
@@ -1,58 +1,54 @@
 class Rover
-
   def initialize(x_coordinate, y_coordinate, dir)
     @x_coordinate = x_coordinate
     @y_coordinate = y_coordinate
     @dir = dir
   end
+end
 
   def read
+    puts "How large is the plateau size?"
+    plateau_size = gets.chomp
 
-  end
+    puts "Where is Rover 1 starting?"
+    starting_position = gets.split(" ")
 
-  def move
+    @x_coordinate = starting_position[0]
+    @y_coordinate = starting_position[1]
+    @dir = starting_position[2]
 
-    case direction
-    when Rover dir == N
-      y_coordinate += 1
-    when Rover dir == E
-      x_coordinate += 1
-    when Rover dir == S
-      y_coordinate -= 1
-    when Rover dir == W
-      x_coordinate -= 1
+    puts "Please give me directions to the drill site."
+    instructions = gets.split(//)
+
+    instructions.each do |instruction|
+      case
+        when instruction == M then move
+        when direction == L || direction == R then turn
+      end
     end
 
   end
 
-  def turn
-
-    case turning
-    when Rover dir == N
-      if instruction == L
-        dir = W
-      else
-        dir = E
-      end
-    when Rover dir == E
-      if instruction == L
-        dir = N
-      else
-        dir = S
-      end
-    when Rover dir == S
-      if instruction == L
-        dir = E
-      else
-        dir = W
-      end
-    when Rover dir == W
-      if instruction == L
-        dir = S
-      else
-        dir = N
-      end
+  def move
+    case instruction
+      when @dir == N then @y_coordinate += 1
+      when @dir == E then @x_coordinate += 1
+      when @dir == S then @y_coordinate -= 1
+      when @dir == W then @x_coordinate -= 1
+    end
   end
 
+  def turn
+    case instruction
+      when @dir == N && instruction == L then @dir = W
+      when @dir == N && instruction == R then @dir = E
+      when @dir == E && instruction == L then @dir = N
+      when @dir == E && instruction == R then @dir = S
+      when @dir == S && instruction == L then @dir = E
+      when @dir == S && instruction == R then @dir = W
+      when @dir == W && instruction == L then @dir = S
+      when @dir == W && instruction == R then @dir = N
+    end
+  end
 
-end
+  # 1 = Rover.new
